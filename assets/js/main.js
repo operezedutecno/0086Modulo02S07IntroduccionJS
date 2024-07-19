5fc298c95be55f926c6fd1a1315743a6fb502c19
Osman Pérez <operez@edutecno.com>
2024-07-18 20:42:26 -0400

    Ejemplo de variables constantes y condicionales

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// alert("Incluir Javascript desde archivo")
+
+const porcRet = 5
+
+porcRet = 6
+
+console.log("Procentaje", porcRet);
+
+var nombre = "José"
+var apellido = "Pérez"
+var edad = 34
+var activo = true
+var mensaje = ""
+
+if(activo) {
+    mensaje = ", SI se encuentra activo"
+} else {
+    mensaje = ", NO se encuentra activo"
+}
+
+console.log("El colaborador "+nombre+" "+apellido+" tiene "+edad+" años"+mensaje)
